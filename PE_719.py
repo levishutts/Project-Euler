6e9565e2bdbb165aa1a531f4fdadd36639bc06e2
levishutts <levicshutts@hotmail.com>
2020-08-17 22:06:17 -0700

    Update PE_719.py
    Slight optimization made by ditching the recursive function that created ALL permutations of each perfect square, n, BEFORE checking if any permutation sum = root.
    Now if a permutation = root, add n to total and go to next perfect square.
    else: check next permutation.

--- PE_719.py
+++ PE_719.py
@@ -1,4 +1,21 @@
+'''
+Module to solve the problem here https://projecteuler.net/problem=719
+
+An S-number is defined as a natural number, n, that is a perfect square AND its
+square root can be obtained by splitting n's digits in 2 or more place and summed up.
+e.g. 81 is an S-number because sqrt(81) = 8 + 1 = 9.
+
+T(N) is the sum of all S-numbers 0 < N. The goal is to find T(10^12).
+'''
+
+
 import math
+from itertools import chain, combinations
+
+"""
+This function is recursive and returns ALL possible permutations causing
+serious issues when n > 10^9
+
 def splitter(str):
     for i in range(1, len(str)):
         start = str[0:i]
@@ -8,23 +25,43 @@ def splitter(str):
             result = [start]
             result.extend(split)
             yield result
-            
+"""
+
+def sliceable(xs):
+    #Return a sliceable version of the iterable xs.
+    try:
+        xs[:0]
+        return xs
+    except TypeError:
+        return tuple(xs)
+
+def partition(iterable):
+    #Returns the next permutation of n
+    '''
+    TODO:
+    if any of the sub-ints are greater than root, go to next permutation.
+    '''
+    s = sliceable(iterable)
+    n = len(s)
+    b, mid, e = [0], list(range(1, n)), [n]
+    getslice = s.__getitem__
+    splits = (d for i in range(n) for d in combinations(mid, i))
+    return [[s[sl] for sl in map(slice, chain(b, d), chain(d, e))]
+            for d in splits]
+
 def sqsplit(n):
     root = math.sqrt(n)
     #Check if perfect square
     if int(root + .5) ** 2 == n:
         
-        #Find all permutations
-        combinations = list(splitter(str(n)))
-        #print(n, combinations)
-        
-        #If any the sum of any permutations = n, add to total
-        for i in combinations:
+        #Check each permutation of digits
+        for i in partition(str(n)): 
+            #If any the sum the current permutation = root, return n
             sum = 0
             for j in i:
                 sum += int(j)
             if sum == root:
-                #print(n, root, combinations)
+                #Return S-number
                 return n
     return 0
             
